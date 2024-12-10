0561fe7ad0532cae06bb995f2ababae3395c8bbf
GabrielJonatas <gabrieljonatas2000@hotmail.com>
2024-12-10 11:43:13 -0300

    commit inicial

---
+++ src/main/java/com/generation/travelWithMe/TravelWithMeApplication.java
@@ -0,0 +1,13 @@
+package com.generation.travelWithMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelWithMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelWithMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/travelWithMe/TravelWithMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.travelWithMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelWithMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
